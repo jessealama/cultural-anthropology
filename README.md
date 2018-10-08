936d7abc3a9cd2d88a68b30a98030f79282ec0d1
Jesse Alama <jesse@lisp.sh>
2018-10-08 07:19:50 +0200

    Let's get this party started

---
+++ README.md
@@ -0,0 +1,11 @@
+cultural-anthropology
+==========
+
+This package bundles all the Racket packages discussed or mentioned by the survey respondents in the book [_Language-oriented Programming in Racket: A Cultural Anthropology_](https://languagemakers.net/anthropology/ "homepage for Language-oriented Programming in Racket: A Cultural Anthropology").
+
+Installation
+-------
+
+Use `raco`:
+
+    $ raco pkg install cultural-anthropology
---
+++ info.rkt
@@ -0,0 +1,62 @@
+#lang info
+
+(define collection "cultural-anthropology")
+
+(define version "1.0.0")
+
+(define deps
+  (list "auxiliary-macro-context"
+        "bitsyntax"
+        "bookcover"
+        "brag"
+        "cmx"
+        "datalog"
+        "dssl"
+        "dssl2"
+        "event-lang"
+        "frog"
+        "frtime"
+        "gm-pepm-2018"
+        "gregor"
+        "gtp-measure"
+        "gtp-paper"
+        "hackett"
+        "herbie"
+        "heresy"
+        "identikon"
+        "ipoe"
+        "lipics"
+        "margrave"
+        "monad"
+        "nanopass"
+        "neuron"
+        "non-det"
+        "parenlog"
+        "pict3d"
+        "plai"
+        "play"
+        "pollen"
+        "pop-pl"
+        "py-fizz"
+        "racket-dogstatsd"
+        "racketscript"
+        "rackjure"
+        "ragg"
+        "rash"
+        "reactor"
+        "redex"
+        "remix"
+        "rosette"
+        "send-exp"
+        "slideshow"
+        "slideshow-exe"
+        "syndicate"
+        "turnstile"
+        "typed-racket"
+        "video"
+        "vr-lang"
+        "web-server"))
+
+(define pkg-desc "All packages discussed or mentioned in the book Language-oriented Programming in Racket: A Cultural Anthropology")
+
+(define pkg-authors '("jesse@lisp.sh"))
